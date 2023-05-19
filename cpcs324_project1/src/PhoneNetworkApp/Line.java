0388982b3305505f4fe84f615902505f1bbb1995
rabbitRM <102517158+rabbitRM@users.noreply.github.com>
2023-05-19 09:13:54 +0300

    Update Line.java

--- cpcs324_project1/src/PhoneNetworkApp/Line.java
+++ cpcs324_project1/src/PhoneNetworkApp/Line.java
@@ -1,5 +1,4 @@
 
-
 package PhoneNetworkApp;
 
 import GraphFramework.Edge;
@@ -7,17 +6,23 @@ import GraphFramework.Edge;
 import GraphFramework.Vertex;
 public class Line extends Edge{
     
-    private static int lLength ;
+    private int lLength ;
     
     
-    public Line( Vertex source1, Vertex target1 , int weight1) {
-       super ( source1 , target1 , weight1);
-       lLength = weight1 *5 ;
+    public Line( Vertex source, Vertex target , int weight) {
+       super ( source , target , weight);
+       lLength = weight *5;
     }
     
     
-    public int diplayInfo(){
-        return lLength;
-    }
     
+    public String displayInfo(){
+        return "line length: "+ lLength ;
+    }
+
+    @Override
+    public int getWeight() {
+       return lLength;
+    }
+
 }
