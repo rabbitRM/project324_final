3685cb8e21f601d0bbd63aa103ea38e7d0012259
rabbitRM <102517158+rabbitRM@users.noreply.github.com>
2023-05-19 04:11:49 +0300

    Update PhoneNWDesignApp.java

--- cpcs324_project1/src/PhoneNetworkApp/PhoneNWDesignApp.java
+++ cpcs324_project1/src/PhoneNetworkApp/PhoneNWDesignApp.java
@@ -1,10 +1,10 @@
 package PhoneNetworkApp;
 
-import GraphFramework.Graph;
 import GraphFramework.KruskalAlg;
-import java.io.FileNotFoundException;
 import GraphFramework.MHprimAlg;
 import GraphFramework.MSTAlgorithm;
+import java.io.FileNotFoundException;
+
 
 
 public class PhoneNWDesignApp {
@@ -15,11 +15,12 @@ public class PhoneNWDesignApp {
 
 
       
-        KruskalAlg k = new KruskalAlg();
+      
         BluePrintsGraph PhLNetwork = new BluePrintsGraph();
         MSTAlgorithm.graph = PhLNetwork;
 
-         MHprimAlg f2 = new MHprimAlg();
+         MHprimAlg f2 = new MHprimAlg(MSTAlgorithm.graph);
+       KruskalAlg k = new KruskalAlg(MSTAlgorithm.graph);
         // long variables to store the strating time and ending time 
         //of processing Kruskal and Prim algorithms
         long startTime, endTime;
@@ -28,42 +29,53 @@ public class PhoneNWDesignApp {
         //in nanoseconed between startTime and endTime variables
         double result;
 
-  MSTAlgorithm.graph.readGraphFromFile("input.txt");
+        
+        
+        
+//      MSTAlgorithm.graph.readGraphFromFile("input.txt");
+//    f2.primMST();
+// 
+//     k.kruskal();
+
+MSTAlgorithm.graph.makeGraph(100, 1000 , 0);  
+//
+ f2.primMST();
+ 
+ k.kruskal();
+       
+
+
+    
      
-  f2.primMinHeap(MSTAlgorithm.graph.vertices);       
-  k.kruskal(MSTAlgorithm.graph.vertices);
-//        
-     MSTAlgorithm.graph.makeGraph(10, 100);
-     
-//        // computing the starting time for the algorithm
-       startTime =  System.currentTimeMillis() ;
-//  
+        // computing the starting time for the algorithm
+//       startTime =  System.currentTimeMillis() ;
+       
 //        // calling prim Algorithm        
-      f2.primMinHeap(MSTAlgorithm.graph.vertices);
+//      
+////        // computing the ending time for the algorithm
+//        endTime = System.currentTimeMillis() ;
+////
+////        // computing the time it took the algorithm to process
+//       result = (double) (endTime - startTime) ;
+//////
+//////        //print a message
+////       System.out.println("It took me " + result + " ms\n");
+//
+//        
+//        // computing the starting time for the algorithm
+//        startTime = System.currentTimeMillis() ;
+//        
+        // calling Kruskal Algorithm        
+//        k.kruskal(MSTAlgorithm.graph);
+        
 //        // computing the ending time for the algorithm
-        endTime = System.currentTimeMillis() ;
+//        endTime = System.currentTimeMillis() ;
 //
 //        // computing the time it took the algorithm to process
-       result = (double) (endTime - startTime) ;
+//        result = (double) (endTime - startTime) ;
 //
 //        //print a message
-       System.out.println("It took me " + result + " ms\n");
-
-        
-        // computing the starting time for the algorithm
-        startTime = System.currentTimeMillis() ;
-        
-        // calling Kruskal Algorithm        
-        k.kruskal(MSTAlgorithm.graph.vertices);
-        
-        // computing the ending time for the algorithm
-        endTime = System.currentTimeMillis() ;
-
-        // computing the time it took the algorithm to process
-        result = (double) (endTime - startTime) ;
-
-        //print a message
-        System.out.println("It took me " + result + " ms\n");
+//        System.out.println("It took me " + result + " ms\n");
     }
 
 }
